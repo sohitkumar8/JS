2c0aa14f6ac094ad8c2630a06db587be4767817f
sohitkumar8 <samratsohit@gmail.com>
2026-06-18 12:44:14 +0530

    variable practice Q. add

---
+++ Variables/practice.js
@@ -0,0 +1,28 @@
+//  1.name variable mein apna naam store karke print karo.
+
+let name = "Sohit";
+console.log(name);
+
+//  2.age variable mein apni age store karke print karo.
+
+let age = 21;
+console.log(age);
+
+
+//  3.city variable banao aur value update karke print karo.
+
+let city = "Patna";
+city = "Gayaa";
+console.log(city);
+
+//  4.const country = "India" banao aur uski value change karne ki koshish karo. Kya hota hai?
+
+const country = "India";
+// country = "USA";    Error aa rha hai.
+console.log(country);  
+
+//  5.num1 aur num2 variable banao aur unka sum print karo.
+
+let num1 = 20;
+let num2 = 30;
+console.log(num1 + num2);
